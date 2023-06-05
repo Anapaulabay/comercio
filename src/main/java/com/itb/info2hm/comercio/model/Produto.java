887961c2f20a2fa823cb3161bfc680314df4ce79
Anapaulabay <oliveiraanapaula6012@gmail.com>
2023-06-05 09:14:04 -0300

    Inicio controle de versao

---
+++ src/main/java/com/itb/info2hm/comercio/model/Produto.java
@@ -0,0 +1,14 @@
+package com.itb.info2hm.comercio.model;
+
+public class Produto {
+
+	private long id;
+	private String nome;
+	private String codigoBarras;
+	private double preco;
+	
+	//public : acesso liberado para todas as classes
+	//private : propriedade (atributos ou metodos)
+	//      dentro da proprio classe
+	
+}
